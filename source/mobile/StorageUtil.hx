28978a42eeeff2664c00314c20fb57ec559f799b
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-04 17:03:02 -0300

    Atualizar o StorageUtil.hx

--- source/mobile/StorageUtil.hx
+++ source/mobile/StorageUtil.hx
@@ -149,8 +149,8 @@ class StorageUtil
 enum abstract StorageType(String) from String to String
 {
 	final forcedPath = '/storage/emulated/0/';
-	final packageNameLocal = 'com.yourname.psychengine';
-	final fileLocal = 'PsychEngine';
+	final packageNameLocal = 'com.ander.termuxengine';
+	final fileLocal = 'TermuxEngine';
 
 	var EXTERNAL_DATA = "EXTERNAL_DATA";
 	var EXTERNAL_OBB = "EXTERNAL_OBB";
